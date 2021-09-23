03424f03d56a4a87b9ad086c5b217e2280c1c736
Siv-tspab <sbapst.j@gmail.com>
2021-09-23 22:09:33 +0200

    FIX: little config bug on leisure category

--- src/Entity/LeisureCategory.php
+++ src/Entity/LeisureCategory.php
@@ -14,7 +14,7 @@ use Doctrine\ORM\Mapping as ORM;
 #[ApiResource(
     paginationEnabled: false,
     collectionOperations: ['get'],
-    itemOperations: []
+    itemOperations: ['get']
 )]
 class LeisureCategory
 {
